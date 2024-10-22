34aea9fd40dcedab74b9cac17f96c185f0aca5cc
Md. Abir Hasan <abirhasanshuvo789@gmail.com>
2024-10-22 18:34:25 +0600

    completed

--- lib/model/news_model.dart
+++ lib/model/news_model.dart
@@ -2,12 +2,25 @@ class NewsModel {
   final String author;
   final String title;
   final String imageUrl;
+  final String content;
+  final String description;
+  final String publishedAt;
 
   NewsModel(
-      {required this.author, required this.title, required this.imageUrl});
+      {required this.author,
+      required this.title,
+      required this.imageUrl,
+      required this.content,
+      required this.description,
+      required this.publishedAt});
 
   factory NewsModel.fromMap(Map<String, dynamic> e) {
     return NewsModel(
-        author: e['author'], title: e['title'], imageUrl: e['urlToImage']);
+        author: e['author'],
+        title: e['title'],
+        imageUrl: e['urlToImage'],
+        content: e['content'],
+        description: e['description'],
+        publishedAt: e['publishedAt']);
   }
 }
--- lib/p_screen/p_screen.dart
+++ lib/p_screen/p_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+
 import 'package:google_fonts/google_fonts.dart';
-import 'package:newspaper/const/const.dart';
-import 'package:newspaper/home_screen/home_screen.dart';
-import 'package:newspaper/news_screen/news_screen.dart';
+
+import 'package:newspaper/model/news_model.dart';
+
+import 'package:newspaper/services/news_api.dart';
 
 class PScreen extends StatefulWidget {
   const PScreen({super.key});
@@ -14,18 +15,44 @@ class PScreen extends StatefulWidget {
 
 class _PScreenState extends State<PScreen> {
   var currentPosition = 0;
+  List<NewsModel> news = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchNews();
+  }
+
+  Future<void> fetchNews() async {
+    var response;
+
+    if (currentPosition == 0) {
+      response = await NewsApi.fetchNews(
+          "https://newsapi.org/v2/top-headlines?sources=al-jazeera-english&apiKey=c0cb83d6ac054269b5e2f4c397b898b9");
+    }
+    if (currentPosition == 1) {
+      response = await NewsApi.fetchNews(
+          "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=c0cb83d6ac054269b5e2f4c397b898b9");
+    } else {
+      response = await NewsApi.fetchNews(
+          "https://newsapi.org/v2/top-headlines?sources=al-jazeera-english&apiKey=c0cb83d6ac054269b5e2f4c397b898b9");
+    }
+
+    setState(() {
+      news = response;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     List<String> items = [
+      'Al Jazeera',
       'BBC',
-      'Explore',
-      'Search',
-      'Feed',
-      'Post',
-      'Activity',
-      'Settings',
-      'Profile'
+      'CNN',
+      'Google News',
+      'CNBC',
+      'New York Post',
+      'NDTV News',
     ];
 
     return Scaffold(
@@ -52,7 +79,7 @@ class _PScreenState extends State<PScreen> {
                         onTap: () {
                           setState(() {
                             currentPosition = index;
-                            print(currentPosition);
+                            fetchNews();
                           });
                         },
                         child: Container(
@@ -92,9 +119,95 @@ class _PScreenState extends State<PScreen> {
                 }),
           ),
 
+          const SizedBox(
+            height: 7,
+          ),
+
           //main screen start from here
 
-          Expanded(child: NewsScreen(indexNumber: currentPosition))
+          Expanded(
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  physics: const BouncingScrollPhysics(),
+                  itemCount: news.length,
+                  itemBuilder: (context, index) {
+                    final newsJ = news[index];
+
+                    return Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 5),
+                      margin: const EdgeInsets.only(right: 5),
+                      child: Stack(
+                        // alignment: AlignmentDirectional.bottomStart,
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(15),
+                            child: Image.network(
+                                fit: BoxFit.cover,
+                                height: 300,
+                                width: 250,
+                                newsJ.imageUrl),
+                          ),
+                          Positioned(
+                            top: 150,
+                            child: Container(
+                              padding: const EdgeInsets.all(2),
+                              margin: const EdgeInsets.only(left: 2, right: 2),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Colors.white,
+                              ),
+                              width: 250,
+                              child: Text(
+                                maxLines: 2,
+                                newsJ.title,
+                                style: GoogleFonts.lato(),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  })),
+
+          const SizedBox(
+            height: 3,
+          ),
+
+          Expanded(
+              child: ListView.builder(
+                  physics: const BouncingScrollPhysics(),
+                  itemCount: news.length,
+                  itemBuilder: (context, index) {
+                    final newInd = news.length / 2;
+                    final newsP = news[index];
+                    return Column(
+                      children: [
+                        // index == 0
+                        //     ? Text('asdasdas')
+                        //     : ListTile(
+                        //         title: Text(newsP.title),
+                        //       ),
+
+                        index >= 3
+                            ? ListTile(
+                                title: Text(
+                                  maxLines: 3,
+                                  newsP.title,
+                                  style: GoogleFonts.merriweather(),
+                                ),
+                                leading: Image.network(
+                                    height: 80, width: 80, newsP.imageUrl),
+                                subtitle: Text(
+                                  newsP.author,
+                                  style: GoogleFonts.lato(),
+                                ),
+
+                                // trailing: Text(newsP.publishedAt),
+                              )
+                            : SizedBox()
+                      ],
+                    );
+                  })),
         ],
       ),
     );
